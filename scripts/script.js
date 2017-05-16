7892471eda0dcf79e41f6737234a7adcee6a718c
Kenji Hirabayashi <mkhira2@gmail.com>
2017-05-16 15:53:38 -0500

    fixed bug

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,39 @@ var handleResponse = function(apiResponse) {
   var cardOneValue = apiResponse.cards[0].value
   var cardTwoValue = apiResponse.cards[1].value
 
+  if (cardOneValue === 'JACK') {
+    cardOneValue = 11
+  }
+  if (cardTwoValue === 'JACK') {
+    cardTwoValue = 11
+  }
+  if (cardOneValue === 'QUEEN') {
+    cardOneValue = 12
+  }
+  if (cardTwoValue === 'QUEEN') {
+    cardTwoValue = 12
+  }
+  if (cardOneValue === 'KING') {
+    cardOneValue = 13
+  }
+  if (cardTwoValue === 'KING') {
+    cardTwoValue = 13
+  }
+  if (cardOneValue === 'ACE') {
+    cardOneValue = 14
+  }
+  if (cardTwoValue === 'ACE') {
+    cardTwoValue = 14
+  }
+
+  else {
+    cardOneValue = parseInt(cardOneValue)
+    cardTwoValue = parseInt(cardTwoValue)
+  }
+  console.log(cardOneValue)
+  console.log(cardTwoValue);
+
+
   console.log(apiResponse.cards)
   containerNode.innerHTML = '<div class="cardBoxes"><div class="firstBox"><p>Player One: </p><img class="card cardOne" src=' + cardOne + '></div>' +
                             '<div class="secondBox"><p>Player Two: </p><img class="card cardTwo" src=' + cardTwo + '></div></div>'
